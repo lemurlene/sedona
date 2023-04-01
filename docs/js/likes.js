586b9d235d711bb831b77b1351ea4a479a536a6f
lemurlene <lenagorelikova4560@gmail.com>
2023-04-01 19:24:18 +0300

    + some availability, sprite, likes-color

--- docs/js/likes.js
+++ docs/js/likes.js
@@ -3,9 +3,10 @@ let likes_number = Array.from(document.querySelectorAll('.foto__likes-number'));
 
 likes.forEach((button, i) => {
   button.addEventListener("click", () => {
-    button.classList.toggle("added");
+    button.classList.toggle("foto__likes--love");
     let current = Number(likes_number[i].innerHTML);
-    let inc = button.classList.contains("added") ? 1 : -1;
-    likes_number[i].innerHTML = current + inc;
+    let count = button.classList.contains("foto__likes-icon--love") ? 1 : -1;
+    likes_number[i].innerHTML = current + count;
+    likes_number.classList.toggle("foto__likes-number--love");
   });
 });
--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,12 @@
 const navToggle = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav");
 
-navToggle.addEventListener("click", function (evt) {
+navToggle.addEventListener("click", (evt) => {
   evt.preventDefault();
   mainNav.classList.toggle("main-nav--closed");
+  let n = "true" === navToggle.getAttribute("aria-expanded");
+    navToggle.setAttribute("aria-expanded", !n),
+        n
+            ? navToggle.setAttribute("aria-label", "Открыть меню")
+            : navToggle.setAttribute("aria-label", "Закрыть меню");
 })
