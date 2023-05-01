081aac22413d512dc9a111db50304af3f6dd5e1c
lemurlene <lenagorelikova4560@gmail.com>
2023-05-01 16:40:38 +0300

    +docs

---
+++ docs/js/script.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".main-nav__toggle");
+const mainNav = document.querySelector(".main-nav");
+const toggleLabel = document.querySelector(".main-nav__toggle-label");
+
+navToggle.classList.add('main-nav__toggle--js');
+
+navToggle.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  mainNav.classList.toggle("main-nav--closed");
+  toggleLabel.textContent === 'Закрыть меню' ? toggleLabel.textContent = 'Открыть меню' : toggleLabel.textContent = 'Закрыть меню';
+})
