bd5a8ebc55fc5615def28cb96edc57bc61a24b03
lemurlene <lenagorelikova4560@gmail.com>
2023-05-05 18:42:06 +0300

    optimization

--- gulpfile.js
+++ gulpfile.js
@@ -83,9 +83,9 @@ exports.images = copyImages;
 // WebP
 
 const createWebp = () => {
-  return gulp.src("source/img/**/*.{jpg,png}")
+  return gulp.src("source/img/foto/*.{jpg,png}")
     .pipe(webp({ quality: 90 }))
-    .pipe(gulp.dest("build/img"))
+    .pipe(gulp.dest("build/img/foto"))
 }
 
 exports.createWebp = createWebp;
