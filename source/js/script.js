9ce9288a36b0b65719a5a762fed1b30a431e4d0c
lemurlene <lenagorelikova4560@gmail.com>
2023-05-31 18:41:20 +0300

    correction mobile navigation

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav");
 const toggleLabel = document.querySelector(".main-nav__toggle-label");
+const headerLogo = document.querySelector(".page-header__logo");
 
 
 mainNav.classList.add('main-nav--closed');
@@ -9,5 +10,7 @@ navToggle.classList.add('main-nav__toggle--js');
 navToggle.addEventListener("click", (evt) => {
   evt.preventDefault();
   mainNav.classList.toggle("main-nav--closed");
+  mainNav.classList.toggle("main-nav--js");
+  headerLogo.classList.toggle('page-header__logo--js');
   toggleLabel.textContent === 'Закрыть меню' ? toggleLabel.textContent = 'Открыть меню' : toggleLabel.textContent = 'Закрыть меню';
 })
